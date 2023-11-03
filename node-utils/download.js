0dc206ffe1cccc5f8e5493866812fe23c3ceb56e
db28ff71f01f0a28ed1727f9375339bc <145771758+db28ff71f01f0a28ed1727f9375339bc@users.noreply.github.com>
2023-11-04 00:52:32 +0900

    Update download.js

--- node-utils/download.js
+++ node-utils/download.js
@@ -1,39 +1,42 @@
 const { exit } = require("process");
-const axios = require('axios').default;
-const fs = require('fs');
+const EasyDl = require("easydl");
 
-(async function (){
-  var args = process.argv.slice(2);
-  if (args.length != 3) {
-    console.log("Usage: download.js DOWNLOAD_URL KEY OUTPUT_FILE");
-    exit(1);
-  }
-  
-  var downloadUrl = args[0];
-  var key = args[1];
-  var outputFile = args[2];
-  
-  await downloadFile(downloadUrl, key, outputFile);
-}());
-
-
-async function downloadFile(url, key, outputFile) {
-  const { data, headers } = await axios({
-      url: url,
-      method: "GET",
-      headers: {
-        "Authorization": "Bearer " + key
-      },
-      responseType: "stream",
-  });
-
-  const contentLength = headers['content-length'];
-
-  console.log(contentLength)
-
-  data.on('data', (chunk) => {
-      // console.log(chunk)
-  })
-
-  data.pipe(fs.createWriteStream(outputFile))
+var args = process.argv.slice(2);
+if (args.length != 3) {
+  console.log("Usage: download.js DOWNLOAD_URL KEY OUTPUT_FILE");
+  exit(1);
 }
+
+var downloadUrl = args[0];
+var key = args[1];
+var outputFile = args[2];
+
+var totalBytes = 0;
+
+new EasyDl(downloadUrl, outputFile, {
+  connections: 6,
+  maxRetry: 5,
+  httpOptions: {
+    headers: {
+      Authorization: "Bearer " + key,
+    },
+  },
+})
+  .on("metadata", function (metadata) {
+    totalBytes = metadata.size;
+  })
+  .on("progress", function (data) {
+    console.log(
+      data.total.bytes,
+      totalBytes,
+      `${data.total.percentage.toFixed(2)}%`
+    );
+  })
+  .on("error", function (err) {
+    console.log(err.message);
+    exit(3);
+  })
+  .on("end", function () {
+    console.log("Download finished!");
+  })
+  .wait();
